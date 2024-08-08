e783fc58319cc0a256d723ce3d7b1ab9e658f057
TejashreePanchal <trpanchal@myseneca.ca>
2024-08-08 13:01:42 -0400

    Update Security and CORS configurations

--- src/main/java/com/example/huko/mongoDB/controllers/ShowController.java
+++ src/main/java/com/example/huko/mongoDB/controllers/ShowController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/shows")
 public class ShowController {
--- src/main/java/com/example/huko/mongoDB/controllers/UserController.java
+++ src/main/java/com/example/huko/mongoDB/controllers/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
